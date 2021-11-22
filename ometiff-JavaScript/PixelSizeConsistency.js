d6a835f1bc7ebfa8c12946cc1ed159a73e017472
Etienne Bergeron <etienne-1.bergeron@polymtl.ca>
2021-11-22 14:31:03 -0500

    PixelSizeConsistency.js update

--- ometiff-JavaScript/PixelSizeConsistency.js
+++ ometiff-JavaScript/PixelSizeConsistency.js
@@ -7,13 +7,24 @@ function getExif() {
 }
 getExif().then(function(result) {
     var xml = result['ImageDescription']['description']
-    console.log("\n OME-XML:", xml)
+    //console.log("\n OME-XML:", xml)
     
 
     var parseString = xml2js.parseString;
 
     parseString(xml, function (err, output) {
-        console.dir(output['OME']['Image'][0]['Pixels'][0]['$']);
+        //console.dir(output['OME']['Image'][0]['Pixels'][0]['$'])
+        console.log("\nOME-TIFF metadata\n---------------------")
+        console.log("PhysicalSizeX:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeX']);
+        console.log("PhysicalSizeXUnit:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeXUnit']);
+        console.log("PhysicalSizeY:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeY']);
+        console.log("PhysicalSizeYUnit:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeYUnit']);
+        console.log("PhysicalSizeZ:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeZ']);
+        console.log("PhysicalSizeZUnit:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeZUnit']);
+        console.log("DimensionOrder:", output['OME']['Image'][0]['Pixels'][0]['$']['DimensionOrder']);
     });
 })
 
+//fetch("test_image.json")
+//  .then(response => response.json())
+//  .then(json => console.log(json));
