dcb136f969abb27f786602cbcb9e279622d76b55
Yuan Wang <wang17@ualberta.ca>
2021-11-28 16:06:20 -0700

    Complete the consistency test script

--- ometiff-JavaScript/PixelSizeConsistency.js
+++ ometiff-JavaScript/PixelSizeConsistency.js
@@ -1,30 +1,56 @@
-import ExifReader from '../../bids-validator/node_modules/exifreader/dist/exif-reader.js'
-import xml2js from '../../bids-validator/node_modules/xml2js/lib/xml2js.js'
+const ExifReader = require('exifreader')
+const parseString = require('xml2js').parseString
+
 
 function getExif() {
-    const tags = ExifReader.load("test_image.ome.tif");
+    const tags = ExifReader.load("test_image.ome.tif")
     return tags
 }
+
+function convertFactor(physicalSizeUnit) {
+    if(physicalSizeUnit === 'mm') {
+        return 1000
+    }else if(physicalSizeUnit === 'um') {
+        return 1
+    }else if(physicalSizeUnit === 'nm') {
+        return 0.001
+    }
+}
+
 getExif().then(function(result) {
     var xml = result['ImageDescription']['description']
     //console.log("\n OME-XML:", xml)
     
-
-    var parseString = xml2js.parseString;
-
     parseString(xml, function (err, output) {
-        //console.dir(output['OME']['Image'][0]['Pixels'][0]['$'])
-        console.log("\nOME-TIFF metadata\n---------------------")
-        console.log("PhysicalSizeX:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeX']);
-        console.log("PhysicalSizeXUnit:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeXUnit']);
-        console.log("PhysicalSizeY:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeY']);
-        console.log("PhysicalSizeYUnit:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeYUnit']);
-        console.log("PhysicalSizeZ:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeZ']);
-        console.log("PhysicalSizeZUnit:", output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeZUnit']);
-        console.log("DimensionOrder:", output['OME']['Image'][0]['Pixels'][0]['$']['DimensionOrder']);
-    });
-})
 
-//fetch("test_image.json")
-//  .then(response => response.json())
-//  .then(json => console.log(json));
+        const physicalSizeX = output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeX']
+        const physicalSizeXUnit = output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeXUnit']
+        const physicalSizeY = output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeY']
+        const physicalSizeYUnit = output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeYUnit']
+        const physicalSizeZ = output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeZ']
+        const physicalSizeZUnit = output['OME']['Image'][0]['Pixels'][0]['$']['PhysicalSizeZUnit']
+
+        console.log("\nOME-TIFF metadata\n---------------------")
+        console.log("PhysicalSizeX:", physicalSizeX)
+        console.log("PhysicalSizeXUnit:", physicalSizeXUnit)
+        console.log("PhysicalSizeY:", physicalSizeY)
+        console.log("PhysicalSizeYUnit:", physicalSizeYUnit)
+        console.log("PhysicalSizeZ:", physicalSizeZ)
+        console.log("PhysicalSizeZUnit:", physicalSizeZUnit)
+        console.log("DimensionOrder:", output['OME']['Image'][0]['Pixels'][0]['$']['DimensionOrder'])
+
+        let factorX = convertFactor(physicalSizeXUnit);
+        let factorY = convertFactor(physicalSizeYUnit);
+        let factorZ = convertFactor(physicalSizeZUnit);
+        let jsonData = require('./test_image.json')
+
+        let pixelSize = jsonData['PixelSize']
+
+        if (physicalSizeX * factorX !== pixelSize[0] || physicalSizeY * factorY !== pixelSize[1] || physicalSizeZ * factorZ !== pixelSize[2]) {
+            console.log("PixelSize is not consistent with PhysicalSizeX, PhysicalSizeY and PhysicalSizeZ OME metadata fields")
+        }else{
+            console.log("PixelSize is consistent with PhysicalSizeX, PhysicalSizeY and PhysicalSizeZ OME metadata fields")
+        }
+
+    })
+})
